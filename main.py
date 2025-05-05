ef66d35e8b1adb01be7399181d2efc74df7f1362
Thatoneperon <Thatoneperon1@gmail.com>
2025-05-05 16:22:17 -0700

    added new window with button and combobox. mainwindow hides when the 200 response code is received but stays up if any other code recieved

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import json
-import http.client
 import sys
 import threading
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
 import requests
 
+class connectedWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
+        self.setFixedWidth(800)
+        self.setFixedHeight(600)
+
+        self.button = QtWidgets.QPushButton("Connect")
+        self.dropdown = QtWidgets.QComboBox()
+
+        self.myframe2 = QtWidgets.QFrame()
+        self.myframe2.setFrameStyle(QtWidgets.QFrame.Shape.StyledPanel | QtWidgets.QFrame.Shadow.Plain)
+
+        button2layout = QtWidgets.QHBoxLayout(self.myframe2) 
+        button2layout.addWidget(self.button)
+        button2layout.addWidget(self.dropdown)
+
+        self.setLayout(button2layout)
+        self.show()
 
 class discordBot(QtWidgets.QWidget):
     def __init__(self):
@@ -19,11 +35,10 @@ class discordBot(QtWidgets.QWidget):
         palette = QtGui.QPalette()
         palette.setBrush(QtGui.QPalette.ColorRole.Window, QtGui.QBrush(sImage))
         self.setPalette(palette)
-        
-        
-
+               
         self.setFixedWidth(800)
         self.setFixedHeight(600)
+        self.w = None
         self.button = QtWidgets.QPushButton("Submit")
         self.label = QtWidgets.QLabel("Enter key:")
         self.input_box = QtWidgets.QLineEdit(self)
@@ -31,66 +46,57 @@ class discordBot(QtWidgets.QWidget):
         self.label.setMaximumHeight(20)
         self.label.setMinimumWidth(75)
         self.label.setAlignment(QtGui.Qt.AlignmentFlag.AlignCenter)
-        
-        
-        
 
         self.myframe = QtWidgets.QFrame()
         self.myframe.setFrameStyle(QtWidgets.QFrame.Shape.StyledPanel | QtWidgets.QFrame.Shadow.Plain)
         
-
         buttonlayout = QtWidgets.QHBoxLayout(self.myframe) 
         buttonlayout.addWidget(self.label)
         buttonlayout.addWidget(self.input_box)
         buttonlayout.addWidget(self.button)
 
-        
-
         self.setLayout(buttonlayout)
         self.show()
         
-        #self.button.clicked.connect(handletext(self))
+        #self.button.clicked.connect(handletext(self)
         self.button.clicked.connect(self.buttonClick)
     
     def buttonClick(self):
         text = self.input_box.text()
-        sendtext(text)
+        handletext(self, text)
         self.input_box.setText("")
 
 
-
-def handletext(self):
-    text_thread = threading.Thread(target=sendtext(self), daemon=True)
+def handletext(self, text):
+    text_thread = threading.Thread(target=sendtext(self, text), daemon=True)
     text_thread.start()
 
-   
-def sendtext(text):
-    
+
+def sendtext(self, text):
 
     data = {
         'method': 'auth',
         'auth': text
-
     }
 
     json_data = json.dumps(data)
 
     try:
-        
-        conn = http.client.HTTPConnection("127.0.0.1", 8000)
-        conn.request("POST", "/", body=json_data, headers={'Content-Type': 'application/json'})
-        #response = requests.post(url, data=json.dumps(self), headers={'Content-Type': 'application/json'})
-
-        response = conn.getresponse()
-        print(response)
-        
-
+        response = requests.post("http://127.0.0.1:8000", data=json_data, headers={'Content-Type': 'application/json'})
+        print(response)    
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-
     except Exception as e:
         print(f"A System error has occured: {e}", file=sys.stderr)
 
+    if response.status_code == 200:
+        open_connectedWindow(self)
+
+def open_connectedWindow(self):
+    self.hide()
+    if self.w is None:
+        self.w = connectedWindow()
+    self.w.show()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
